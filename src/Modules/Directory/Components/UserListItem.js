969819a014afa621d214302a2d2cfe61c791ce6b
corbettg <corbettg@uncw.edu>
2018-07-25 17:29:54 -0400

    changes

--- src/Modules/Directory/Components/UserListItem.js
+++ src/Modules/Directory/Components/UserListItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { updateProfileID_and_ActiveView } from '../../../ReduxStore/Actions'
 import { connect } from 'react-redux'
 
-import { Avatar, Button, Divider, Grid, Paper, Typography } from '@material-ui/core'
+import { Avatar, Button, Divider, Grid, Icon, Paper, Typography } from '@material-ui/core'
 
 const mapStateToProps = (state) => {
     return { session: state.session }
@@ -12,10 +12,10 @@ class UserListItem extends Component {
 
     render() {
         return (
-            <Grid item xl={4} lg={6} md={6} sm={12} xs={12} style={{marginTop:10, marginBottom:10}} >
-                <Paper style={{boxShadow: 'none'}}>
+            <Grid item xl={4} lg={4} md={4} sm={12} xs={12} style={{padding:"2%"}}>
+                <Paper elevation={4} style={{padding: "10px 10px"}}>
                     <Grid container>
-                        <Grid item xl={6} lg={6} md={6} sm={12} xs={12} align="center" style={{marginTop:10, marginBottom:10}} >
+                        <Grid item xl={12} lg={12} md={12} sm={12} xs={12} align="center" style={{marginTop:10, marginBottom:10}} >
                             { this.props.avatar_path ? 
                                 <Avatar alt={this.props.first_name} 
                                         src={'https://sleepy-plateau-42917.herokuapp.com' + this.props.avatar_path}
@@ -23,12 +23,22 @@ class UserListItem extends Component {
                                 <Avatar alt='No Avatar' src='/images/avatars/no_avatar.png'
                                         style={{width:150, height:150}} /> }
                         </Grid>
-                        <Grid item xl={6} lg={6} md={6} sm={12} xs={12} align="left" style={{marginTop:10, marginBottom:10}} >
+                        <Grid item xl={12} lg={12} md={12} sm={12} xs={12} align="center" style={{marginTop:10, marginBottom:10}} >
                             <Typography variant='headline'>
                                 {this.props.first_name} {this.props.last_name}
                             </Typography>
                             <Divider style={{marginTop:10, marginBottom:10, width:'90%'}} />
-                            <Button color="primary" variant="raised" size="medium" style={{textTransform:"capitalize"}}
+
+                            <Typography variant='body1' align="left">
+                                <Icon style={{paddingLeft: 5}}>mail</Icon> {this.props.email}
+                            </Typography>
+
+                            <Typography variant='body1' align="left">
+                                <Icon style={{paddingLeft: 5}}>phone</Icon> {this.props.cell_phone_number}
+                            </Typography>
+
+                            <Button color="primary" variant="raised" size="medium" 
+                                    style={{textTransform:"capitalize", fontWeight: "bold", backgroundColor: "#FF8500" }}
                                     onClick={(e) => 
                                     this.props.dispatch(updateProfileID_and_ActiveView(this.props.id, 'Directory', 'Profile'))}>
                                     View Profile</Button>
--- src/Modules/Directory/Views/Users.js
+++ src/Modules/Directory/Views/Users.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { UserListItem } from '../Components'
 import { connect } from 'react-redux'
 import { fetchUsersWithRedux } from '../../../ReduxStore/Actions'
-import { FormControl, Grid, TextField } from '@material-ui/core'
+import { FormControl, Grid, Paper, TextField } from '@material-ui/core'
 
 const mapStateToProps = (state) => {
     return { session: state.session, users: state.users }
@@ -38,18 +38,21 @@ class Users extends Component {
             <div>
                 <Grid container>
                 <h1>{this.prpos}</h1>
-                    <Grid item xl={12} lg={12} md={12} sm={12} xs={12}>
-                        <FormControl fullWidth>
-                            <TextField id="searchTerm" label="Search Directory" margin="normal"
-                                    value={this.state.searchTerm} 
-                                    onChange={this.handleChange('searchTerm')}
-                                    />
-                        </FormControl>
+                    <Grid item xl={12} lg={12} md={12} sm={12} xs={12} style={{padding:"1% 2%"}}>
+                        <Paper elevation={4} style={{padding: "10px 10px"}}>
+                            <FormControl fullWidth>
+                                <TextField id="searchTerm" label="Search Directory" margin="normal"
+                                        value={this.state.searchTerm} 
+                                        onChange={this.handleChange('searchTerm')}
+                                        />
+                            </FormControl>
+                        </Paper>
                     </Grid>
                 </Grid>
-                <Grid container spacing={8} >
+                <Grid container spacing={40} style={{padding:"1% 2%"}}>
                     {this.props.users.map(data => ( <UserListItem key={data.id} id={data.id}
-                                avatar_path={data.avatar_path} first_name={data.first_name} last_name={data.last_name} />
+                                avatar_path={data.avatar_path} first_name={data.first_name} last_name={data.last_name}
+                                email={data.email} cell_phone_number ={data.cell_phone_number} />
                     ))}
                 </Grid>
             </div>
