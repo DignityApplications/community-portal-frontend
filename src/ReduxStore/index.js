90edd8f9136ffd20b56181ae2132a53fcf5ccea9
corbettg <corbettg@uncw.edu>
2018-10-02 17:32:53 -0400

    production build fixed

--- src/ReduxStore/index.js
+++ src/ReduxStore/index.js
@@ -1,7 +1,7 @@
 import appReducer from './Reducers'
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 //Middleware can be added here
 
