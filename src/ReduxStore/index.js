c7b15b288fdbf4c3d282a91c8d37fa5ec6a711b5
corbettg <corbettg@uncw.edu>
2018-10-02 17:17:18 -0400

    devtools_update

--- src/ReduxStore/index.js
+++ src/ReduxStore/index.js
@@ -6,7 +6,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 //Middleware can be added here
 
 export default (initialState={}) => {
-	if (process.env.NODE_ENV !== 'production') {
+	if (process.env.NODE_ENV === 'development') {
 		return applyMiddleware(thunk)(createStore)(appReducer, initialState, composeWithDevTools())
 	} else {
 		return applyMiddleware(thunk)(createStore)(appReducer, initialState)
